6a7245d43b52cbc5a87eeab8a82862bcd1e9502d
Diogo Luís Pietrobiazzi <diogopietrobiazzi@gmail.com>
2026-06-01 22:08:00 -0300

    [Diogo] Refactor: Extract Superclass for Duplicate Code in pasture_call_handler.lua

--- call_handlers/pasture_call_handler.lua
+++ call_handlers/pasture_call_handler.lua
@@ -1,53 +1,56 @@
-local validator = radiant.validator
-
-local PastureCallHandler = class()
+local BaseCallHandler = require 'stonehearth_ace.call_handlers.base_call_handler'
+local PastureCallHandler = class(BaseCallHandler)
 
 function PastureCallHandler:set_pasture_harvest_animals_renewable(session, response, pasture, value)
-   validator.expect_argument_types({'Entity', 'boolean'}, pasture, value)
-
-   if session.player_id ~= pasture:get_player_id() then
+   if not self:is_player_entity_authorized(session, pasture) then
       return false
-   else
-      local pasture_component = pasture:get_component('stonehearth:shepherd_pasture')
+   end
+
+   local pasture_component = pasture:get_component('stonehearth:shepherd_pasture')
+   if pasture_component then
       pasture_component:set_harvest_animals_renewable(value)
       return true
    end
+   return false
 end
 
 function PastureCallHandler:set_pasture_harvest_grass(session, response, pasture, value)
-   validator.expect_argument_types({'Entity', 'boolean'}, pasture, value)
-
-   if session.player_id ~= pasture:get_player_id() then
+   if not self:is_player_entity_authorized(session, pasture) then
       return false
-   else
-      local pasture_component = pasture:get_component('stonehearth:shepherd_pasture')
+   end
+
+   local pasture_component = pasture:get_component('stonehearth:shepherd_pasture')
+   if pasture_component then
       pasture_component:set_harvest_grass(value)
       return true
    end
+   return false
 end
 
 function PastureCallHandler:set_pasture_collect_strays(session, response, pasture, value)
-   validator.expect_argument_types({'Entity', 'boolean'}, pasture, value)
-
-   if session.player_id ~= pasture:get_player_id() then
+   if not self:is_player_entity_authorized(session, pasture) then
       return false
-   else
-      local pasture_component = pasture:get_component('stonehearth:shepherd_pasture')
+   end
+
+   local pasture_component = pasture:get_component('stonehearth:shepherd_pasture')
+   if pasture_component then
       pasture_component:set_collect_strays_enabled(value)
       return true
    end
+   return false
 end
 
 function PastureCallHandler:set_pasture_maintain_animals(session, response, pasture, value)
-   validator.expect_argument_types({'Entity', 'number'}, pasture, value)
-
-   if session.player_id ~= pasture:get_player_id() then
+   if not self:is_player_entity_authorized(session, pasture) then
       return false
-   else
-      local pasture_component = pasture:get_component('stonehearth:shepherd_pasture')
+   end
+
+   local pasture_component = pasture:get_component('stonehearth:shepherd_pasture')
+   if pasture_component then
       pasture_component:set_maintain_animals(value)
       return true
    end
+   return false
 end
 
 return PastureCallHandler
